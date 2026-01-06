7105a40db2b3184bd8f82dcecf0aa0384aa6d287
Pavel Mareš <164063945+painter99@users.noreply.github.com>
2026-01-06 22:05:33 +0100

    Add files via upload

---
+++ sandbox/2026-01-06.py
@@ -0,0 +1,27 @@
+todos = []
+
+while True:
+    user_action = input("Type add, show, edit, complete or exit: ")
+    user_action = user_action.strip()
+
+    match user_action:
+        case "add":
+            todo = input("Enter a todo: ")
+            todos.append(todo)
+        case "show":
+            for index, item in enumerate(todos):
+                row = f"{index + 1}-{item}"
+                print(row)
+        case "edit":
+            number = int(input("Enter the number of the todo to edit: "))
+            number = number - 1
+            new_todo = input("Enter the new todo: ")
+            todos[number] = new_todo
+        case "complete":
+            number = int(input("Enter the number of the todo to complete: "))
+            number = number - 1
+            todos.pop(number)
+        case "exit":
+            break
+
+print("Goodbye!")
